c466d5625d1b9a8e04638f23411db56cc2488349
wangke1996 <porkw@outlook.com>
2020-05-28 18:02:49 +0800

    add empty line in the end

--- wmt20/lib/io_helper.py
+++ wmt20/lib/io_helper.py
@@ -1,5 +1,19 @@
 import os
+import sys
+import time
 import numpy as np
+import json
+
+
+def save_json(data, file):
+    with open(file, 'w', encoding='utf8') as f:
+        json.dump(data, f)
+
+
+def load_json(file):
+    with open(file, 'r', encoding='utf8') as f:
+        data = json.load(f)
+    return data
 
 
 def load_txt_file(file, convert2array=True):
@@ -33,7 +47,7 @@ def write_tokens_to_file(tokens, file):
 
 def write_lines(lines, file):
     with open(file, 'w', encoding='utf8') as f:
-        f.write('\n'.join([str(x).strip() for x in lines]))
+        f.write('\n'.join([str(x).strip() for x in lines])+'\n')
 
 
 def generate_odps_table(in_file, out_file, src_lang='en', tgt_lang='de', delimiter='\005', id_prefix=None):
@@ -49,6 +63,18 @@ def generate_odps_table(in_file, out_file, src_lang='en', tgt_lang='de', delimit
         f.write('\n'.join([delimiter.join(x) for x in table]))
 
 
+def shuf_in_order(folder, in_files, delimiter='\005'):
+    merged_file = os.path.join(folder, 'merged.tmp')
+    merged_lines = map(lambda x: delimiter.join(x),
+                       zip(*[load_txt_file(os.path.join(folder, f), False) for f in in_files]))
+    write_lines(merged_lines, merged_file)
+    os.system('shuf %s > %s' % (merged_file, merged_file + '.shuf'))
+    out_lines = zip(*map(lambda x: x.split(delimiter), load_txt_file(merged_file + '.shuf', False)))
+    for file, lines in zip(in_files, out_lines):
+        out_file = os.path.join(folder, file + '.shuf')
+        write_lines(lines, out_file)
+
+
 def run_cmd(cmd, throw_error=False):
     return_code = os.system(cmd)
     if return_code != 0:
@@ -57,3 +83,8 @@ def run_cmd(cmd, throw_error=False):
             raise RuntimeError(error_log)
         else:
             print(error_log)
+
+
+def print_info_with_time(s):
+    print('%s: %s' % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), s))
+    sys.stdout.flush()
